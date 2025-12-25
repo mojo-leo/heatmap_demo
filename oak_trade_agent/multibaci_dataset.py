08322e2f7813adfadc2e206d0452069c4d70f519
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2025-12-25 15:53:43 +0800

    Added a stub for making a real multi-year time series

---
+++ oak_trade_agent/multibaci_dataset.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+###############################################################################
+class MultiBaciDataset:
+    """Regroups several BACI datasets into a single object in order to have
+    a time series of trade data."""
+
+    pass
