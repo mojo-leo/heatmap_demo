99bc5c5a541c500585982af4da2157ee5daec1bf
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2025-12-25 15:16:10 +0800

    Create __main__.py

---
+++ oak_trade_agent/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# The main object of our package
+from oak_trade_agent import XXXX
+
+# The main function to run when we are called #
+def main():
+    xx = XXXX(x)
+    return xx()
+
+# Execute when run, not when imported
+if __name__ == "__main__": main()
