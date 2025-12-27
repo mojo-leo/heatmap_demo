a7160c0446b957816ba6e16e73251c96eb5c8215
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2025-12-27 16:54:13 +0800

    Tui.chart is very nice! Just missing the scale

---
+++ oak_trade_agent/with_js/tui_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Internal modules
+from oak_trade_agent.with_js.template import TemplateHTML
+
+
+###############################################################################
+class TUIStatic(TemplateHTML):
+    template_name = "tui_static.html"
+
+
+###############################################################################
+tui_static = TUIStatic()
+if __name__ == "__main__":
+    tui_static()
